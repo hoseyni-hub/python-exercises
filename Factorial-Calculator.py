e69e31d73c6f9fc161c0235d259549d80d9ae864
hoseyni-hub <hoseyn.business@gmail.com>
2025-08-21 15:29:44 +0330

    Add Factorial Calculator program

---
+++ Factorial-Calculator.py
@@ -0,0 +1,10 @@
+# Factorial Calculator
+# This program calculates the factorial of a given number.
+
+num = int(input("Enter a number: "))
+fact = 1
+
+for i in range(1, num + 1):
+    fact *= i
+
+print(f"Factorial of {num} is: {fact}")
