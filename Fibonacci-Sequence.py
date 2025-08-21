8d757f2d04b1d43391273d7515cb83c638c8b4f5
hoseyni-hub <hoseyn.business@gmail.com>
2025-08-21 15:31:16 +0330

    Add Fibonacci Sequence generator

---
+++ Fibonacci-Sequence.py
@@ -0,0 +1,12 @@
+# Fibonacci Sequence
+# This program prints Fibonacci sequence up to n terms.
+
+n = int(input("Enter the number of terms: "))
+
+a, b = 0, 1
+count = 0
+
+while count < n:
+    print(a, end=" ")
+    a, b = b, a + b
+    count += 1
