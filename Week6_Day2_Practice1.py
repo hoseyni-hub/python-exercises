10328f7a384333db68eca5088d7a6a8e516ad322
hoseyni-hub <hoseyn.business@gmail.com>
2025-08-26 20:32:26 +0330

    Add Week 6 Day 2 - Practice 1 (Palindrome Checker)

---
+++ Week6_Day2_Practice1.py
@@ -0,0 +1,11 @@
+# Week 6 - Day 2 - Practice 1
+# Palindrome Checker
+
+def is_palindrome(word):
+    word = word.lower().replace(" ", "")
+    return word == word[::-1]
+
+# Example usage:
+print(is_palindrome("level"))   # True
+print(is_palindrome("hello"))   # False
+print(is_palindrome("Never odd or even"))  # True
