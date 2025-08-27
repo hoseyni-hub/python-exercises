c96eb105e42cf1aacee1206656ab5e48355def67
hoseyni-hub <hoseyn.business@gmail.com>
2025-08-27 13:50:39 +0330

    Add week 6 exercise 5 - Reverse String

---
+++ week6_exercise5_reverse_string.py
@@ -0,0 +1,9 @@
+# Program to reverse a string
+
+def reverse_string(text):
+    return text[::-1]
+
+# Test the function
+word = "Canada"
+print("Original word:", word)
+print("Reversed word:", reverse_string(word))
