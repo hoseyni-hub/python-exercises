de2898bfac521e82728c1d903d412db64f7d26e7
hoseyni-hub <hoseyn.business@gmail.com>
2025-10-07 15:31:22 +0330

    Add files via upload

---
+++ calculator/main_module_example.py
@@ -0,0 +1,7 @@
+from operations import add, subtract, multiply, divide
+
+if __name__ == "__main__":
+    print("Addition:", add(5, 3))
+    print("Subtraction:", subtract(5, 3))
+    print("Multiplication:", multiply(5, 3))
+    print("Division:", divide(10, 2))
